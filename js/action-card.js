0eaa3160c146542180358a1d09f5fa7148168485
kingce <chuan.e.king@gmail.com>
2021-10-20 01:12:02 -0700

    saving

---
+++ js/action-card.js
@@ -0,0 +1,19 @@
+$(document).ready(() => {
+
+    $('#card-one').click(() => { 
+        TweenMax.to("#card-one", 1, {right: 0, width: "0"});
+    });
+
+    $('#card-two').click(() => { 
+        TweenMax.to("#card-two", 1, {right: 0, width: "0"});
+    });
+
+    $('#card-three').click(() => { 
+        TweenMax.to("#card-three", 1, {right: 0, width: "0"});
+    });
+
+    $('#card-four').click(() => { 
+        TweenMax.to("#card-four", 1, {right: 0, width: "0"});
+    });
+
+});
